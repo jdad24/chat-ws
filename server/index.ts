a8bc1d701f7290cdf741e78fe87dea952012f708
Joshua Dadson <joshuadadson@yahoo.com>
2026-04-12 18:25:47 -0500

    redis

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import WebSocket, { WebSocketServer } from 'ws';
 import dotenv from 'dotenv';
 import express from 'express';
+import { createClient } from 'redis';
 
 dotenv.config();
 const PORT: number | undefined = Number(process.env.PORT) || 8080;
@@ -12,22 +13,45 @@ const expressServer = app.listen(PORT, '0.0.0.0', () => {
     console.log(`HTTP server is running on ${addressInfo?.address}:${addressInfo?.port}`);
 });
 
+const wsClients = new Set<WebSocket>();
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
+
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+redisClient.connect().then(() => {
+    console.log('Connected to Redis');
+}).catch((err) => {
+    console.error('Failed to connect to Redis:', err);
+});
+
+await redisClient.subscribe('chat-messages', (message) => {
+    console.log(`Received message from Redis channel: ${message}`);
+    // Broadcast the message to all connected WebSocket wsClients
+    for (let client of wsClients) {
+        if (client.readyState === client.OPEN) {
+            client.send(message);
+        }
+    }
+});
+
 
 const webSocketServer = new WebSocketServer({ server: expressServer });
 
-const clients = new Set<WebSocket>();
-
 webSocketServer.on('connection', (socket: WebSocket) => {
     console.log('Client connected');
-    clients.add(socket);
+    wsClients.add(socket);
 
-    socket.on('message', (message: WebSocket.Data) => {
+    socket.on('message', async (message: WebSocket.Data) => {
         console.log(`Received message: ${message}`);
-        // Broadcast the message to all connected clients
+        // Broadcast the message to all connected wsClients
 
-        for (let client of clients) {
+        for (let client of wsClients) {
             if (client === socket && client.readyState === client.OPEN) {
                 // client.send("Back from server: " + message.toString());
+                await redisClient.publish('chat-messages', message.toString());
                 console.log(`Echoing back to sender: ${message}`);
             }
         }
@@ -35,6 +59,6 @@ webSocketServer.on('connection', (socket: WebSocket) => {
 
     socket.on('close', () => {
         console.log('Client disconnected');
-        clients.delete(socket);
+        wsClients.delete(socket);
     });
 });
