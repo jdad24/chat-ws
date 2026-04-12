bf8e6ac46152cd2b2db1ce6c28af48d662a6068f
Joshua Dadson <joshuadadson@yahoo.com>
2026-04-12 14:44:22 -0500

    any type

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,14 @@
-import WebSocket, {WebSocketServer } from 'ws';
-import { AddressInfo } from 'net';
+import WebSocket, { WebSocketServer } from 'ws';
 import dotenv from 'dotenv';
 import express from 'express';
 
 dotenv.config();
 const PORT: number | undefined = Number(process.env.PORT) || 8080;
-let addressInfo: AddressInfo | string | null = null;
+let addressInfo: any = null;
 
 const app = express()
 const expressServer = app.listen(PORT, '0.0.0.0', () => {
-    addressInfo = expressServer.address() as AddressInfo | null;
+    addressInfo = expressServer.address() as any;
     console.log(`HTTP server is running on ${addressInfo?.address}:${addressInfo?.port}`);
 });
 
@@ -19,22 +18,22 @@ const webSocketServer = new WebSocketServer({ server: expressServer });
 const clients = new Set<WebSocket>();
 
 webSocketServer.on('connection', (socket: WebSocket) => {
-  console.log('Client connected');
-  clients.add(socket);
+    console.log('Client connected');
+    clients.add(socket);
 
-  socket.on('message', (message: WebSocket.Data) => {
-    console.log(`Received message: ${message}`);
-    // Broadcast the message to all connected clients
-    
-    for(let client of clients) {
-        if(client === socket && client.readyState === client.OPEN) {
-            client.send("Back from server: " + message.toString());
-        }   
-    }
-  });
+    socket.on('message', (message: WebSocket.Data) => {
+        console.log(`Received message: ${message}`);
+        // Broadcast the message to all connected clients
 
-  socket.on('close', () => {
-    console.log('Client disconnected');
-    clients.delete(socket);
-  });
+        for (let client of clients) {
+            if (client === socket && client.readyState === client.OPEN) {
+                client.send("Back from server: " + message.toString());
+            }
+        }
+    });
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+        clients.delete(socket);
+    });
 });
